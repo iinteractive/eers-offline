4a84ea83cbb1b5a71d75e645bf9f1b1b7c2e84d2
Stevan Little <stevan.little@iinteractive.com>
2011-07-14 21:30:42 -0400

    returning to Net::SFTP

--- lib/EERS/Offline/Transporter/SFTP.pm
+++ lib/EERS/Offline/Transporter/SFTP.pm
@@ -2,7 +2,7 @@
 package EERS::Offline::Transporter::SFTP;
 use Moose;
 
-use Net::SFTP::Foreign;
+use Net::SFTP;
 use File::Spec::Functions;
 
 our $VERSION = '0.01';
@@ -20,34 +20,34 @@ has '_sftp_handle' => (
     clearer => '_clear_sftp_handle',
     default => sub {
         my $self = shift;
-        Net::SFTP::Foreign->new($self->host,
+        Net::SFTP->new($self->host,
             user     => $self->username,
             password => $self->password,
-        );        
+        );
     }
 );
 
 sub put {
     my ($self, %params) = @_;
-    
+
     my $source_path      = catfile($self->source_dir_path,      $params{source});
     my $destination_path = catfile($self->destination_dir_path, $params{destination});
-        
+
     try {
         my $sftp = $self->_sftp_handle;
 
         if ($sftp->error) {
             die "Unable to connect to host(" . $self->host . ")";
         }
-        
+
         unless ($sftp->put($source_path, $destination_path)) {
-            die 'Cannot PUT file (from => ' 
-                        . $source_path 
-                        . ', to => ' 
+            die 'Cannot PUT file (from => '
+                        . $source_path
+                        . ', to => '
                         . $destination_path
                         . ')';
-        } 
-    } 
+        }
+    }
     catch {
         $self->error('SFTP Error: ' . $@);
         $self->_clear_sftp_handle;
